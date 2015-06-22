518abe9fc9530d0c5edf771e2dfce456dd9b1aa0
Eric <ericscottlagergren@gmail.com>
2015-06-21 20:43:53 -0700

    spaces -> tabs

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ function initialize() {
 		zoom: 13, // something like 8 for entire state.
 		// center: {lat: 47.2500, lng: -120.1000} // WA coords.
 		center: {lat: 47.7717, lng: -122.2044}
-  	};
+	};
 
-  	map = new google.maps.Map(document.getElementById('map-canvas'),
-  		mapOptions);
+	map = new google.maps.Map(document.getElementById('map-canvas'),
+		mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
